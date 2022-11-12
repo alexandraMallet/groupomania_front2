8ac2d0dd66fe722381b50c30116832415ba0a73a
Alexandra Mallet <malletalexandra@gmail.com>
2022-11-12 17:44:19 +0100

    index.js Login correction

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const routes = [
   {
@@ -9,7 +10,7 @@ const routes = [
   },
   {
     path: '/login',
-    name: 'login',
+    name: 'LoginView',
     component: LoginView
   },
 ]
