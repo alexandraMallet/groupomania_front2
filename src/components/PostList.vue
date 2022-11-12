e88baa6e19041eaf839e983024c4603527c0e7c3
Alexandra Mallet <malletalexandra@gmail.com>
2022-11-12 17:48:40 +0100

    PostList component

---
+++ src/components/PostList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="posts">
+      <p>Ici s'afficheront toutes les publications</p>
+      <PostCard></PostCard>
+      <PostCard></PostCard>
+      <PostCard></PostCard>
+      <PostCard></PostCard>
+      <PostCard></PostCard>
+      <!-- <PostCard v-for="post in posts" :key="post.id" :event="post" /> -->
+    </div>
+  
+  </template>
+  
+  <script>
+      import PostCard from '@/components/PostCard.vue';
+      export default {
+          name: 'PostList',
+          components: {
+              PostCard,
+          }
+      }
+  </script>
+  
+  <style scoped lang="scss">
+  .posts {
+      width: 95%;
+      height: fit-content;
+      margin: 10px;
+      border: 1px solid #FD2D01;
+  }
+  </style>
