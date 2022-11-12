fab34fa4903d4b270a70f433fc0d2ff4bf014adf
Alexandra Mallet <malletalexandra@gmail.com>
2022-11-12 17:34:52 +0100

    Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button>{{ buttonText }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    buttonText: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  height: 30px;
+  width: 100px;
+  display: flex;
+  text-align: center;
+  background-color: #FD2D01;
+  color: #4E5166;
+  border-radius: 50px;
+  border: none;
+  margin: 20px;
+}
+</style>
