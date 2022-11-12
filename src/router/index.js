e46a6d587c26d19cb4139452e0f78d9cae0556f4
Alexandra Mallet <malletalexandra@gmail.com>
2022-11-12 17:53:51 +0100

    AllPostsUsersView et path et correction nom component PostsList

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import SignupView from '@/views/SignupView.vue'
+import AllPostsUsersView from '@/views/AllPostsUsersView'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/signup',
     name: 'SignupView',
     component: SignupView
+  },
+  {
+    path: 'publications',
+    name: 'allPostsAndUsers',
+    component: AllPostsUsersView
   },
 ]
 
---
+++ src/views/AllPostsUsersView.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="header-app">
+        <nav>
+            <router-link to="/posts">Toutes les publications</router-link> | 
+            <router-link to="/publier">Publier</router-link> | 
+            <router-link to="/">Se déconnecter</router-link>
+        </nav>
+        <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+
+    <div class="global-view">
+        <div class="users-contener">
+            <UsersList />
+        </div>
+        <div class="posts-contener">
+            <PostList />
+        </div>
+    </div>
+
+</template>
+
+<script>
+import UsersList from '@/components/UsersList.vue';
+import PostList from '@/components/PostsList.vue';
+export default {
+    name: 'AllPostsUsersView',
+    components: {
+        PostList,
+        UsersList,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.global-view {
+    display: flex;
+}
+.users-contener {
+    width: 25%;
+    margin-right: 30px;
+    border: 1px solid #4E5166;
+}
+.posts-contener {
+    width: 65%;
+    margin-right: 30px;
+    border: 1px solid #4E5166;
+}
+.header-app {
+    display: flex;
+    justify-content: space-between;
+  }
+nav {
+    padding: 30px;
+    a {
+        font-weight: bold;
+        color: #4E5166;
+        &.router-link-exact-active {
+            color: #FD2D01;
+        }
+    }
+}
+</style>
