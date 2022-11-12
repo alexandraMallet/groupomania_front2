d2cd70978c32e4934c50daabca4dd9c704fcf827
Alexandra Mallet <malletalexandra@gmail.com>
2022-11-12 17:35:51 +0100

    InputEmail component

---
+++ src/components/InputEmail.vue
@@ -0,0 +1,17 @@
+<template>
+    <label for="email">Email :</label>
+    <input class="input-email"/>
+  </template>
+  
+  <script>
+  export default {
+    name: 'InputEmail',
+  };
+  </script>
+  
+  <style scoped lang="scss">
+  input {
+    height: 50px;
+    width: 700px;
+  }
+  </style>
