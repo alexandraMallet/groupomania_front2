337bc511a9adbc128e9f41814e1779e94daa6c02
Alexandra Mallet <malletalexandra@gmail.com>
2022-11-12 17:38:45 +0100

    PostCard component

---
+++ src/components/PostCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="post-card">
+        <p>Ici s'affichera une publication.</p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'PostCard',
+    }
+</script>
+
+<style scoped lang="scss">
+    .post-card {
+        height: fit-content;
+        width: 95%;
+        margin: 20px;
+        border: 1px solid #FFD7D7;
+    }
+</style>
