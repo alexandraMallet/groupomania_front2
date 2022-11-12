b730861881dbf68ebe6300f23d7d79f3b75386ef
Alexandra Mallet <malletalexandra@gmail.com>
2022-11-12 17:36:49 +0100

    InputPassword component

---
+++ src/components/InputPassword.vue
@@ -0,0 +1,17 @@
+<template>
+    <label for="password">Password :</label>
+    <input class="input-password" placeholder="(au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un caractère spécial)"/>
+</template>
+
+<script>
+export default {
+    name: 'InputPassword',
+};
+</script>
+
+<style scoped lang="scss">
+input {
+    height: 50px;
+    width: 700px;
+}
+</style>
