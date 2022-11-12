106d652b29da737d25c789c199d1fe264ce1b9b5
Alexandra Mallet <malletalexandra@gmail.com>
2022-11-12 17:49:48 +0100

    UserCard component

---
+++ src/components/UserCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="user-card">
+        <p>Ici s'affichera l'avatar et le nom d'un.e utilisateurice.</p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name:'UserCard',
+    }
+</script>
+
+<style scoped lang="scss">
+    .user-card {
+        height: fit-content;
+        width: 95%;
+        margin: 20px;
+        border: 1px solid #FFD7D7;
+    }
+</style>
